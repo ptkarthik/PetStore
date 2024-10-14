d65b64020e0a06a67a37da118b085a524db0bce4
Karthik <pt.karthik1989@gmail.com>
2024-10-14 10:09:24 +0530

    first commit

---
+++ src/test/java/org/tests/GetPetTest.java
@@ -0,0 +1,14 @@
+package org.tests;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class GetPetTest {
+
+
+    @Test
+    public void testPetId() {
+        RestAssured.given().baseUri("https://petstore.swagger.io/v2").when().get("/pet/112233").then().log().all();
+    }
+}
